fb47dba63a9a5b489078413fa440c2dc8c1801c0
Mattan Ram <mattanmr@gmail.com>
2026-01-21 18:23:51 +0200

    fix: Handle ExternalServices constant load order in NewRelic initializer
    Check if ExternalServices constant is defined before using it, fallback to
    direct ENV variable check. This fixes initializer load order issues where
    active_record_initializers load before regular initializers.

--- config/active_record_initializers/newrelic_and_unicorn.rb
+++ config/active_record_initializers/newrelic_and_unicorn.rb
@@ -4,6 +4,10 @@
 # 
 # Performance monitoring disabled by default for local development
 # To enable: Set ENABLE_PERFORMANCE_MONITORING=true or ENABLE_EXTERNAL_SERVICES=true
-if defined?(Unicorn) && ExternalServices.performance_monitoring_enabled?
-  ::NewRelic::Agent.after_fork(:force_reconnect => true)
+if defined?(Unicorn)
+  if defined?(ExternalServices) && ExternalServices.performance_monitoring_enabled?
+    ::NewRelic::Agent.after_fork(:force_reconnect => true)
+  elsif ENV['ENABLE_PERFORMANCE_MONITORING'] == 'true' || ENV['ENABLE_EXTERNAL_SERVICES'] == 'true'
+    ::NewRelic::Agent.after_fork(:force_reconnect => true)
+  end
 end
