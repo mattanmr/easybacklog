fdbfc9cc224809369396f13af69c1bf844cb93c5
Mattan Ram <mattanmr@gmail.com>
2026-01-21 18:02:06 +0200

    fix: Add locale options to sign up form and remove blog link from footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Create default locales if they don't exist
+[
+  { :name => 'English (United States)', :code => 'en_US' },
+  { :name => 'English (United Kingdom)', :code => 'en_GB' },
+  { :name => 'German', :code => 'de_DE' },
+  { :name => 'French', :code => 'fr_FR' },
+  { :name => 'Spanish', :code => 'es_ES' },
+  { :name => 'Italian', :code => 'it_IT' }
+].each_with_index do |locale_data, index|
+  Locale.create(locale_data.merge(:position => index + 1)) unless Locale.find_by_code(locale_data[:code])
+end
